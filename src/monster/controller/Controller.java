c3b7099e53d5222f1573e31e9c8d1cbbd57c0319
CAngell0 <carsonjesseangell.learn@gmail.com>
2024-09-30 09:30:06 -0600

    Initialized a monster using the default constructor

--- src/monster/controller/Controller.java
+++ src/monster/controller/Controller.java
@@ -1,7 +1,12 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+
 public class Controller {
    public Controller(){}
 
-   public void start(){}
+   public void start(){
+      MarshmallowMonster demo = new MarshmallowMonster();
+      System.out.println(demo);
+   }
 }
