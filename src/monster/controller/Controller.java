09287e26e6a97581e67b39fcd3066d1d1683ce3d
CAngell0 <carsonjesseangell.learn@gmail.com>
2024-09-30 09:53:04 -0600

    Added a call for the overloaded constructor of MarshmallowMonster

--- src/monster/controller/Controller.java
+++ src/monster/controller/Controller.java
@@ -7,7 +7,7 @@ public class Controller {
 
    public void start(){
       MarshmallowMonster demo = new MarshmallowMonster();
-      System.out.println("The moster has:");
+      System.out.println("The monster has:");
       System.out.println("\t- \"" + demo.getName() + "\" as a name");
       System.out.println("\t- " + demo.getTotalAppandageLength() + " appandage length");
       System.out.println("\t- " + demo.getTailLength() + " tail length");
@@ -15,5 +15,15 @@ public class Controller {
       System.out.println("\t- " + demo.getEyeCount() + " eyes");
       System.out.println("\t- " + demo.getEarCount() + " ears");
       System.out.println("\t- " + (demo.getHasHat() ? "A" : "No") + " hat");
+
+      MarshmallowMonster myMonster = new MarshmallowMonster("Crawly", 4.7, 3.2, 6, 4, 2, false);
+      System.out.println("The monster has:");
+      System.out.println("\t- \"" + myMonster.getName() + "\" as a name");
+      System.out.println("\t- " + myMonster.getTotalAppandageLength() + " appandage length");
+      System.out.println("\t- " + myMonster.getTailLength() + " tail length");
+      System.out.println("\t- " + myMonster.getWingCount() + " wings");
+      System.out.println("\t- " + myMonster.getEyeCount() + " eyes");
+      System.out.println("\t- " + myMonster.getEarCount() + " ears");
+      System.out.println("\t- " + (myMonster.getHasHat() ? "A" : "No") + " hat");
    }
 }
