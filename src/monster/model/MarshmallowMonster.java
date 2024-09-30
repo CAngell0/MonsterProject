507fdeb187370ddee4e387963a98d9f9bafa6e81
CAngell0 <carsonjesseangell.learn@gmail.com>
2024-09-30 08:59:50 -0600

    Added data members to a new MarshmallowMonster class

---
+++ src/monster/model/MarshmallowMonster.java
@@ -0,0 +1,10 @@
+package monster.model;
+
+public class MarshmallowMonster {
+   private double totalAppandageLength;
+   private double tailLength;
+   private int wingCount;
+   private int eyeCount;
+   private int earCount;
+   private boolean hasHat;
+}
